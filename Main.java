be93c9cac6a78dab4866920d79d99fde5e934d83
MrBlueOctopus <70705348+MrBlueOctopus@users.noreply.github.com>
2020-09-03 19:26:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Введите кол-во элементов массива:");
+	Scanner in = new Scanner(System.in);
+	int a=in.nextInt();
+	ArrayList <Integer> arrayList = new ArrayList<>();
+	for (int i=0; i<a; ++i){
+        arrayList.add((int)(Math.random()*100));
+        System.out.print(arrayList.get(i)+" ");
+    }
+	System.out.println();
+	int sum=0, i;
+	for (i=0; i<a; ++i){
+	    sum+= arrayList.get(i);
+    }
+	System.out.println(sum);
+	i=0;
+	sum=0;
+	while (i<a){
+	    sum+=arrayList.get(i);
+	    ++i;
+    }
+	System.out.println(sum);
+	sum =0;
+	i=0;
+	do {
+	    sum+=arrayList.get(i);
+	    ++i;
+    }while (i<a);
+	System.out.println(sum);
+    }
+}
